764b598452bb3a166d71e5ecb2d15b4cc17e240d
raogpip <vitalik.fedorenko.2018@gmail.com>
2025-02-18 12:27:24 +0100

    Display top 10 artists from a given year

--- src/main/scala/spotify/YearlyTopArtists.scala
+++ src/main/scala/spotify/YearlyTopArtists.scala
@@ -3,11 +3,14 @@ package spotify
 import org.apache.spark.sql.{SparkSession}
 import org.apache.spark.sql.functions._
 import spotify.SpotifyDataSchema.SpotifyData
+import java.util.logging.{Level, Logger}
 
 
 object YearlyTopArtists {
 
     def main(args: Array[String]): Unit = {
+
+        Logger.getLogger("org").setLevel(Level.SEVERE)
 
         val spark = SparkSession.builder
             .appName("SpotifyEDA")
@@ -24,6 +27,17 @@ object YearlyTopArtists {
             .withColumn("skipped", col("skipped").cast("boolean"))
             .as[SpotifyData]
 
-        data.printSchema()
+
+        val songsFrom2013 = data.filter(year(to_date(col("ts"))) === 2013)
+            .groupBy(col("artist_name").as("artist"))
+            .agg(round(sum("ms_played") / 1000 / 60 / 60, 2).as("total_hours_played"))
+            .orderBy(desc("total_hours_played"))
+            .show(10)
+        
+        println("^^^ Your Top 10 artists in 2013 ^^^")
+        
+        
+
+        spark.stop()
     }
 }
