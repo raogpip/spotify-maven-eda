d3b22d313251d625ba274322ba6571ac152074db
raogpip <vitalik.fedorenko.2018@gmail.com>
2025-02-18 16:55:40 +0100

    Add Favourites object to analyze and display top tracks, artists, albums, and platforms from Spotify data

---
+++ src/main/scala/spotify/Favourites.scala
@@ -0,0 +1,54 @@
+package spotify
+
+import spotify.SpotifyDataSchema.SpotifyData
+import org.apache.spark.sql.{SparkSession}
+import org.apache.spark.sql.functions._
+import java.util.logging.{Level, Logger}
+
+object Favourites {
+    def main(args: Array[String]): Unit = {
+
+        Logger.getLogger("org").setLevel(Level.SEVERE)
+
+        val spark = SparkSession.builder
+            .appName("Favourites")
+            .master("local[*]")
+            .getOrCreate()
+
+        import spark.implicits._
+        val spotifyData = spark.read
+            .option("header", "true")
+            .csv("data/spotify_history.csv")
+            .withColumn("ms_played", col("ms_played").cast("int"))
+            .withColumn("shuffle", col("shuffle").cast("boolean"))
+            .withColumn("skipped", col("skipped").cast("boolean"))
+            .as[SpotifyData]
+
+        
+        val favouriteTracks = spotifyData.filter(col("skipped") === false)
+            .groupBy(col("track_name").as("track"), col("artist_name").as("artist"))
+            .agg(count("track_name").as("times_played"))
+            .orderBy(desc("times_played"))
+            .show(10)
+
+        val favouriteArtists = spotifyData.filter(col("skipped") === false)
+            .groupBy(col("artist_name").as("artist"))
+            .agg(count("artist_name").as("times_played"))
+            .orderBy(desc("times_played"))
+            .show(10)
+
+        val favouriteAlbums = spotifyData.filter(col("skipped") === false)
+            .groupBy(col("album_name").as("album"))
+            .agg(count("album_name").as("times_played"))
+            .orderBy(desc("times_played"))
+            .show(10)
+
+        val favouritePlatforms = spotifyData.filter(col("skipped") === false)
+            .groupBy(col("platform").as("platform"))
+            .agg(count("platform").as("times_used"))
+            .orderBy(desc("times_used"))
+            .show(10)
+
+        spark.stop()
+    }
+}
