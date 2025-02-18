5a5516fb3379b2e39e247afdae979c17b245e655
raogpip <vitalik.fedorenko.2018@gmail.com>
2025-02-18 14:21:54 +0100

    Add ListensOverTheYears object to analyze Spotify listening data by year

--- src/main/scala/spotify/ListensOverTheYears.scala
+++ src/main/scala/spotify/ListensOverTheYears.scala
@@ -5,21 +5,19 @@ import org.apache.spark.sql.functions._
 import spotify.SpotifyDataSchema.SpotifyData
 import java.util.logging.{Level, Logger}
 
-
-object YearlyTopArtists {
-
-    def main(args: Array[String]): Unit = {
-
+object ListensOverTheYears {
+  
+    def main(args: Array[String]) : Unit = {
+        
         Logger.getLogger("org").setLevel(Level.SEVERE)
 
         val spark = SparkSession.builder
-            .appName("SpotifyEDA")
+            .appName("ListensOverTheYears")
             .master("local[*]")
             .getOrCreate()
 
-
         import spark.implicits._
-        val data = spark.read
+        val spotifyData = spark.read
             .option("header", "true")
             .csv("data/spotify_history.csv")
             .withColumn("ms_played", col("ms_played").cast("int"))
@@ -27,15 +25,12 @@ object YearlyTopArtists {
             .withColumn("skipped", col("skipped").cast("boolean"))
             .as[SpotifyData]
 
+        val listensOverTheYears = spotifyData
+            .groupBy(year(to_date(col("ts"))).as("year"))
+            .agg(round(sum("ms_played")/1000/60/60,2).as("total_hs_played"))
+            .orderBy("year")
+            .show()
 
-        val songsFrom2013 = data.filter(year(to_date(col("ts"))) === 2013)
-            .groupBy(col("artist_name").as("artist"))
-            .agg(round(sum("ms_played") / 1000 / 60 / 60, 2).as("total_hours_played"))
-            .orderBy(desc("total_hours_played"))
-            .show(10)
-        
-        println("^^^ Your Top 10 artists in 2013 ^^^")
-        
         spark.stop()
     }
 }
